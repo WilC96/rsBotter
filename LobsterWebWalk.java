e8cbf3fc49ee98660b8b9903d5b162875c8142ec
WilC96 <47301499+WilC96@users.noreply.github.com>
2019-11-08 09:39:19 +0000

    Update LobsterWebWalk.java

--- LobsterWebWalk.java
+++ LobsterWebWalk.java
@@ -22,34 +22,6 @@ import org.osbot.rs07.utility.ConditionalSleep;
 
 @ScriptManifest(author = "Glaciation96", info = "SecondAttempt", name = "KlobsterWebWalk", version = 0, logo = "")
 
-final class Sleep extends ConditionalSleep {
-
-	private final BooleanSupplier condition;
-
-	public Sleep(final BooleanSupplier condition, final int timeout) {
-		super(timeout);
-		this.condition = condition;
-	}
-
-	public Sleep(final BooleanSupplier condition, final int timeout, final int interval) {
-		super(timeout, interval);
-		this.condition = condition;
-	}
-
-	@Override
-	public final boolean condition() throws InterruptedException {
-		return condition.getAsBoolean();
-	}
-
-	public static boolean sleepUntil(final BooleanSupplier condition, final int timeout) {
-		return new Sleep(condition, timeout).sleep();
-	}
-
-	public static boolean sleepUntil(final BooleanSupplier condition, final int timeout, final int interval) {
-		return new Sleep(condition, timeout, interval).sleep();
-	}
-}
-
 public class LobsterWebWalk extends Script {
 
 	private fishInterpolation fishDelay = new fishInterpolation(random(20, 30), random(1800, 2300));

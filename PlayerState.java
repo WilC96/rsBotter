8732a42c0cd4e6b716fc2972b4ee36674efa8f3a
WilC96 <47301499+WilC96@users.noreply.github.com>
2019-11-20 13:10:22 +0000

    Update and rename KaramFishing.java to PlayerState.java

--- PlayerState.java
+++ PlayerState.java
@@ -1,6 +1,6 @@
 package com.fishing;
 
-public enum KaramFishing {
+public enum PlayerState {
 	SMALLNET(new String[] { "Small fishing net", "Coins" }, "Net", 1521, 621), 
 	BAIT(new String[] { "Fishing rod", "Fishing bait", "Coins" }, "Bait", 1521, 623), 
 	CAGE(new String[] { "Lobster pot", "Coins" }, "Cage", 1522, 619);
@@ -10,7 +10,7 @@ public enum KaramFishing {
 	private final int fishingSpot;
 	private final int fishingAnim;
 
-	KaramFishing(String[] t, String action, int f, int anim) {
+	PlayerState(String[] t, String action, int f, int anim) {
 		this.tool = t;
 		this.action = action;
 		this.fishingSpot = f;
